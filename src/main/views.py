64e90bb02702178b82f3363b3e70a76b627e00b5
tmtakashi <tmtakshi7@gmail.com>
2019-02-16 11:03:03 +0900

    viewの追加

--- src/main/views.py
+++ src/main/views.py
@@ -1,4 +1,13 @@
 from django.views import generic
+from django.shortcuts import render
+
+from instruments.models import Instrument
+from amps.models import Amp
+from pedals.models import Pedal
+
+from instruments.forms import InstrumentForm
+from amps.forms import AmpForm
+from pedals.forms import PedalForm
 
 
 class HomePageView(generic.TemplateView):
@@ -6,4 +15,21 @@ class HomePageView(generic.TemplateView):
 
 
 def add_gear_view(request):
-    return request
+    if request.method == 'POST':
+        instrument_form = InstrumentForm(request.POST)
+        amp_form = AmpForm(request.POST)
+        pedal_form = PedalForm(request.POST)
+        if instrument_form.is_valid() and amp_form.is_valid() and pedal_form.is_valid():
+            instrument = instrument_form.save()
+            amp = instrument_form.save()
+            pedal = pedal_form.save()
+    else:
+        instrument_form = InstrumentForm()
+        amp_form = AmpForm()
+        pedal_form = PedalForm()
+    context = {
+        'instrument_form': instrument_form,
+        'amp_form': amp_form,
+        'pedal_form': pedal_form,
+    }
+    return render(request, 'add_gears.html', context=context)
