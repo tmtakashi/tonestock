e0bc66dd18c17b1d3cb3e7046dccca16b1d8a13f
tmtakashi <tmtakshi7@gmail.com>
2019-02-17 11:00:46 +0900

    ユーザー登録フォームとりあえず完成

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+# ユーザ作成フォームを継承
 
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField()
-    password1 = forms.CharField(widget=forms.widgets.PasswordInput)
-    password2 = forms.CharField(widget=forms.widgets.PasswordInput)
+    email = forms.EmailField()
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
