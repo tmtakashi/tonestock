a4418a7d1946a2cbdb66a8567b2499b6d94711f6
tmtakashi <tmtakshi7@gmail.com>
2019-02-17 23:28:27 +0900

    Ampモデルにパラメータ追加

--- src/amps/models.py
+++ src/amps/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Amp(models.Model):
     AMP_TYPES = [
@@ -13,6 +15,23 @@ class Amp(models.Model):
     brand = models.CharField(max_length=128, verbose_name='ブランド名')
     type = models.CharField(
         max_length=128, choices=AMP_TYPES, verbose_name='アンプの種類')
+
+    bass = models.FloatField(verbose_name='Bass',
+                             validators=[MinValueValidator(
+                                 0), MaxValueValidator(10)]
+                             )
+    middle = models.FloatField(verbose_name='Middle',
+                               validators=[MinValueValidator(
+                                   0), MaxValueValidator(10)]
+                               )
+    treble = models.IntegerField(verbose_name='Treble',
+                                 validators=[MinValueValidator(
+                                     0), MaxValueValidator(10)]
+                                 )
+    presence = models.IntegerField(verbose_name='Presence',
+                                   validators=[MinValueValidator(
+                                       0), MaxValueValidator(10)]
+                                   )
 
     def __str__(self):
         return self.name
--- src/tonestock/settings.py
+++ src/tonestock/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'widget_tweaks',
     'main',
     'users',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'tonestock.urls'
@@ -130,3 +132,26 @@ LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+if DEBUG:
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    # 表示する内容
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
