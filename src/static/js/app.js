1411cde73072ffed8e487c513be785ce710d7145
tmtakashi <tmtakshi7@gmail.com>
2019-04-17 15:50:59 +0900

    単一コンポーネントファイルを読めるようになった

---
+++ src/static/js/app.js
@@ -0,0 +1,7 @@
+window.Vue = require('vue');
+
+new Vue({
+    el: '#test',
+    components: { Test },
+    template: '<test></test>'
+})
---
+++ src/webpack.config.js
@@ -0,0 +1,67 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+var env = process.env.NODE_ENV;
+
+module.exports = {
+    context: __dirname,
+    entry: './static/js/app.js',
+
+    output: {
+        filename: "[name]-[hash].js",
+        path: path.resolve('./static/bundles/'),
+        // publicPath: '/'
+    },
+
+    plugins: [
+        new BundleTracker({ filename: './webpack-stats.json' }),
+        // .vueファイルを読み込めるようにする
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules:  [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "vue-style-loader",
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+ 
+    resolve: {
+        extensions: ['.js', '.vue'],
+        modules: [
+            "node_modules"
+        ],
+        alias: {
+            'vue': path.resolve('./node_modules/vue/dist/vue.js'),
+        }
+    } 
+}
