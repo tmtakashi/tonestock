3d2db1963ca4965f0f541aaa9b4af79386d7350b
tmtakashi <tmtakshi7@gmail.com>
2019-04-19 14:45:07 +0900

    削除機能追加

---
+++ src/static/tones/js/tone-card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-sm-4 my-5">
+    <div class="card tone-card">
+      <div class="card-body">
+        <div class="options">
+          <i
+            class="fas fa-chevron-down"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="true"
+          ></i>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="{% url 'tones:edit_tone' tone.pk %}">編集</a>
+            <span
+              class="dropdown-item"
+              @click="destroy"
+              data-toggle="modal"
+              data-target="#deleteToneModal"
+            >削除</span>
+          </div>
+        </div>
+        <h5>
+          <a class="tone-name" href="{% url 'tones:detail' tone.pk %}">{{ name }}</a>
+        </h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "index"],
+  methods: {
+    destroy: function() {
+      this.$emit("destroy", this.index);
+    }
+  }
+};
+</script>
