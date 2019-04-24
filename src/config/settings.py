455de4d57a1e36264bc188d929c60257802e6fd2
tmtakashi <tmtakshi7@gmail.com>
2019-04-24 15:56:24 +0900

    環境変数周りを設定

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env('.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%u#^nf769&ibpom@x6)s(=x_3(a@4p385-42s%f(*8i!v7yy1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['34.217.81.251']
 
