822558b60e3e36afab553f9f84bd5e2e2689f898
tmtakashi <tmtakshi7@gmail.com>
2019-04-24 16:59:31 +0900

    STATIC_FILES_DIRSを変更

--- src/config/settings.py
+++ src/config/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATIC_ROOT = "/var/www/tonestock/static/"
 
-STATIC_ROOT = "/var/www/static/"
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
 
 # Authentication
 LOGIN_URL = 'users:login'
