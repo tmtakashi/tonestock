ba6d2d34989c88f4ac8bb4ae1bf1f41dc9a7dea5
tmtakashi <tmtakashi7@gmail.com>
2019-05-09 15:56:29 +0900

    開発環境用のポート開放、プロフィール編集画面の変更バグ解消

--- src/config/settings.py
+++ src/config/settings.py
@@ -189,39 +189,3 @@ WEBPACK_LOADER = {
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
-
-LOGGING = {
-    'version': 1,   # これを設定しないと怒られる
-    'formatters': {  # 出力フォーマットを文字列形式で指定する
-        'all': {    # 出力フォーマットに`all`という名前をつける
-            'format': '\t'.join([
-                "[%(levelname)s]",
-                "asctime:%(asctime)s",
-                "module:%(module)s",
-                "message:%(message)s",
-                "process:%(process)d",
-                "thread:%(thread)d",
-            ])
-        },
-    },
-    'handlers': {  # ログをどこに出すかの設定
-        'file': {  # どこに出すかの設定に名前をつける `file`という名前をつけている
-            'level': 'DEBUG',  # DEBUG以上のログを取り扱うという意味
-            'class': 'logging.FileHandler',  # ログを出力するためのクラスを指定
-            'filename': os.path.join(BASE_DIR, 'django.log'),  # どこに出すか
-            'formatter': 'all',  # どの出力フォーマットで出すかを名前で指定
-        },
-        'console': {  # どこに出すかの設定をもう一つ、こちらの設定には`console`という名前
-            'level': 'DEBUG',
-            # こちらは標準出力に出してくれるクラスを指定
-            'class': 'logging.StreamHandler',
-            'formatter': 'all'
-        },
-    },
-    'loggers': {  # どんなloggerがあるかを設定する
-        'command': {  # commandという名前のloggerを定義
-            'handlers': ['file', 'console'],  # 先述のfile, consoleの設定で出力
-            'level': 'DEBUG',
-        },
-    },
-}
